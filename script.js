6afbf0934057060789252d5eb1ff9cbdf34ff1a8
„benearne“ <b.arnold@posteo.de>
2025-05-23 12:17:30 +0200

    set up files, basic js

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+// GameBoard Object {
+// 		// array[9 items, empty in the beginning]
+
+//  Game function () {
+	 
+// 		if array is full and no winner, return game over, no winner
+	
+// 	Player Object (name) {
+// 		this.name = name;
+// 		let turns = 0;
+// 		assign symbol O or X
+// 		player with symbol O = playerRound; 
+		
+// 	function play() {
+// 	playerRound clicks one of the nine buttons = items in array and 			store clicked in array;
+// 	increment turns;
+// 	change PlayRound
+// 	}
+
+// 	function winner() {
+// 	if (playerRound -> in the array: 3 in a row || every third || first and next 	4th twice || third and next second twice) playerRound wins
+// 	}
+	
+// 	if (winner() = true) {
+// 	stop and announce winner, reset game board
+// 	} else play();
+
+// Player1(var = name) is an instance of Player
+// Player 2(var = name) is an instance of Player
+
+		
