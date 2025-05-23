83d93f63a786581c5f50fc673607f0143822b26b
„benearne“ <b.arnold@posteo.de>
2025-05-23 20:43:30 +0200

    set up java script

--- script.js
+++ script.js
@@ -1,33 +1,95 @@
+function Game() {
+    //create array board, empty items
+    const board = Array(9).fill(null)
 
+    let winner;
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],    // rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],    // columns
+        [0, 4, 8], [2, 4, 6]                // diagonals
+    ]
 
-// GameBoard Object {
-// 		// array[9 items, empty in the beginning]
+    function Player(name, sign) {
+        this.name = name;
+        this.sign = sign;
+        this.turns = 0;
+        this.incrementTurns = function() {
+            this.turns++;
+        }
+    }
 
-//  Game function () {
-	 
-// 		if array is full and no winner, return game over, no winner
-	
-// 	Player Object (name) {
-// 		this.name = name;
-// 		let turns = 0;
-// 		assign symbol O or X
-// 		player with symbol O = playerRound; 
-		
-// 	function play() {
-// 	playerRound clicks one of the nine buttons = items in array and 			store clicked in array;
-// 	increment turns;
-// 	change PlayRound
-// 	}
+    const player1 = new Player("Michael", "X");
+    const player2 = new Player("Susan", "O");
+    let currentPlayer = player1;
 
-// 	function winner() {
-// 	if (playerRound -> in the array: 3 in a row || every third || first and next 	4th twice || third and next second twice) playerRound wins
-// 	}
-	
-// 	if (winner() = true) {
-// 	stop and announce winner, reset game board
-// 	} else play();
+    function makeMove(i) {
+        // check board at index isn't free or winner already clear
+        if (board[i] !== null || winner) {
+            console.log("field already taken");
+            return;
+        };
 
-// Player1(var = name) is an instance of Player
-// Player 2(var = name) is an instance of Player
+        // update board with players sign
+        board[i] = currentPlayer.sign;
 
-		
+        // 	increment turns of currentPlayer;
+        currentPlayer.incrementTurns()
+        let choice = board[i];
+
+        // check for winner and if console.log, return
+        if (checkWin()) {
+            console.log(`winner is: ${currentPlayer.name}`);
+            return;
+        }
+
+        // check for a tie, return
+        if (board.every(cell => cell !== null)) {
+            console.log("Game over, it's a tie");
+            return
+        }
+
+        console.log(board);
+
+        // change to the other player
+        switchPlayer()
+    }
+        
+
+    function checkWin() {
+        return winningCombinations.some(([a, b, c]) => {
+            if (
+                board[a] &&
+                board[a] === board[b] &&
+                board[a] === board[c]
+            ) {
+                winner = currentPlayer;
+                return true;
+            }
+            return false;
+        });
+    }
+
+    function switchPlayer() {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    function reset() {
+        board.fill(null);
+        winner = null;
+        player1.turns = 0;
+        player2.turns = 0;
+        currentPlayer = player1;
+        console.log("Game reset");
+    }
+
+    return {
+        makeMove,
+        reset, 
+        getBoard: () => [...board],
+        getCurrentPlayer: () => currentPlayer,
+        getWinner: () => winner
+    }
+
+}
+
+const game = Game();
